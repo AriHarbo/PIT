0050a0cd1509e7d24d33f43cb81ffd6c96395bc1
Lolo <lorenzoaletto28@gmail.com>
2024-10-02 20:10:26 -0300

    Acá agregamos un poquito más a la parte de YT y que quede un toque más lindo. Faltaría organizar bien los tamaños y espacios

--- src/components/ContenedorYT.jsx
+++ src/components/ContenedorYT.jsx
@@ -7,31 +7,38 @@ const ContenedorYT = () => {
 
   useEffect(()=> {
     const apiKey = 'AIzaSyA8DCuLkAnOgAcVmCMyzbe58hNkNJW_dX8';
-    const channelID = 'UC9HvIXXe9x-ohW9oQ02HwrQ';
+    const channelID =  'UCHIwQiZhIn6fm5b_IilqBbQ' ///'UC9HvIXXe9x-ohW9oQ02HwrQ'(id del canal de pit);
     const url = `https://www.googleapis.com/youtube/v3/search?part=id,snippet&channelId=${channelID}&order=date&maxResults=1&key=${apiKey}`;
 
     fetch(url)
     .then(response => response.json())
     .then(data=>{
       const videoID= data.items[0].id.videoId;
-      console.log(data);
       setVideoID(videoID);
     })
     .catch(error => console.error(error));
   },[])
    
     return (
-    <div>
+    <div className='contenedorYT'>
+        <div className='infoCanal'>
+            <h1>Visita nuestro canal de <span className='colorSecundario'>YouTube</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem cumque ut debitis molestiae maiores eum fugiat distinctio repudiandae perspiciatis ducimus, quam reprehenderit expedita quia repellat aut? Rem quas sequi sint.</p>
+        </div>
+        <div className='ultimoVideo'>
        <h1>Ultimo video</h1>
       <iframe
         title="Ultimo video"
-        width="853"
-        height="480"
+        width="800"
+        height="400"
         src={`https://www.youtube.com/embed/${videoID}`}
         frameBorder="0"
         allowFullScreen
         />
+
+        </div>
     </div>
+
   )
 }
 
